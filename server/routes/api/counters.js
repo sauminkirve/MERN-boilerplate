406e505dd4f6e1b39d25233c1ad07dbf054ab4d5
saumin kirve <saumin.k@turtlemint.com>
2018-10-03 21:20:35 +0530

    daily data populate

--- server/routes/api/counters.js
+++ server/routes/api/counters.js
@@ -1,51 +1,35 @@
 const Counter = require('../../models/Counter');
+const request = require('request');
 
 module.exports = (app) => {
-  app.get('/api/counters', (req, res, next) => {
-    Counter.find()
-      .exec()
-      .then((counter) => res.json(counter))
-      .catch((err) => next(err));
+  app.post('/api/stocks', (req, res, next) => {
+
+  var request = require('request');
+  request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=full&apikey=demo', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+
+    const content = JSON.parse(body);
+
+    res.send({
+      success : true,
+      message: 'Its Good',
+      stockInfo : content['Time Series (Daily)']['2018-10-03']
+    });
+   
   });
 
-  app.post('/api/counters', function (req, res, next) {
-    const counter = new Counter();
-
-    counter.save()
-      .then(() => res.json(counter))
-      .catch((err) => next(err));
+  
+   
+      
   });
 
-  app.delete('/api/counters/:id', function (req, res, next) {
-    Counter.findOneAndRemove({ _id: req.params.id })
-      .exec()
-      .then((counter) => res.json())
-      .catch((err) => next(err));
-  });
+  
 
-  app.put('/api/counters/:id/increment', (req, res, next) => {
-    Counter.findById(req.params.id)
-      .exec()
-      .then((counter) => {
-        counter.count++;
+  
 
-        counter.save()
-          .then(() => res.json(counter))
-          .catch((err) => next(err));
-      })
-      .catch((err) => next(err));
-  });
+  
 
-  app.put('/api/counters/:id/decrement', (req, res, next) => {
-    Counter.findById(req.params.id)
-      .exec()
-      .then((counter) => {
-        counter.count--;
-
-        counter.save()
-          .then(() => res.json(counter))
-          .catch((err) => next(err));
-      })
-      .catch((err) => next(err));
-  });
+  
 };
